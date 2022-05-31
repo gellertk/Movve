749f9a514b7d24c045e1153a7b056ef79c37d8f0
Kirill Gellert <Gerry9@bk.ru>
2022-05-31 15:30:05 +0300

    добавил модель

---
+++ Movve/FilmCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  FilmCollectionViewCell.swift
+//  Movve
+//
+//  Created by Кирилл  Геллерт on 31.05.2022.
+//
+
+import UIKit
+
+class FilmCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Movve/Model/Actor.swift
@@ -0,0 +1,15 @@
+//
+//  Actor.swift
+//  Movve
+//
+//  Created by Кирилл  Геллерт on 31.05.2022.
+//
+
+import Foundation
+
+struct Actor: Hashable {
+    
+    let name: String
+    let image: URL
+    
+}
