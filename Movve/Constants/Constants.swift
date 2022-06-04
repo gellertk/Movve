d038234658c54faeaccf3a6a8d8a2139f7016fe5
Frodoname <freeway995@gmail.com>
2022-06-04 11:47:05 +0300

    first commit

--- Movve/Constants/Constants.swift
+++ Movve/Constants/Constants.swift
@@ -17,4 +17,13 @@ struct K {
         
     }
     
+    struct Url {
+        
+        static let urlMovie = "https://api.themoviedb.org/3/discover/movie?api_key=7822759433d26b0333c1c26e943ac26f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate"
+        
+        static let urlTv = "https://api.themoviedb.org/3/discover/tv?api_key=7822759433d26b0333c1c26e943ac26f&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0"
+        
+        static let urlImage = "https://image.tmdb.org/t/p/w300"
+    }
+    
 }
